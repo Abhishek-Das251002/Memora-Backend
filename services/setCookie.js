817d518e07ddbc668726ba6423f13242be7b8e7a
Abhishek <abhishekgautam1966@gmail.com>
2026-06-24 19:54:29 +0530

    correct cookie setting

--- services/setCookie.js
+++ services/setCookie.js
@@ -1,6 +1,8 @@
 const setSecureCookie = (res, token) => {
     res.cookie('jwt_token', token, {
         httpOnly: true,
+        secure: true,
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
     })
 
