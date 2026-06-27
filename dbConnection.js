38b43b59c414d5b9b6fe58d88f7f592c8bc5081d
Abhishek <abhishekgautam1966@gmail.com>
2026-06-27 11:56:14 +0530

    db connection code improvised

--- dbConnection.js
+++ dbConnection.js
@@ -5,18 +5,25 @@ const connectionUri = process.env.MONGODB_URI
 
 async function connectToDb() {
     try {
+        if (mongoose.connection.readyState === 1) {
+            console.log("MongoDB already connected.");
+            return;
+        }
+
+        // Connection in progress
+        if (mongoose.connection.readyState === 2) {
+            console.log("MongoDB connection is already in progress.");
+            return;
+        }
         console.log("connecting to db...")
 
-        const isConnected = await mongoose.connect(connectionUri)
+        await mongoose.connect(connectionUri)
 
-        if(isConnected){
-            console.log("connected to database.")
-            console.log("readyState:", mongoose.connect.readyState)
-        }else{
-            console.log("failed to make Db connection.")
-        }
+        console.log("connected to database.")
+        console.log("readyState:", mongoose.connection.readyState)
     } catch (error) {
-        console.error(error)
+        console.error(error);
+        throw error;
     }
 }
 
